a2df75db9abbc33e1042495f7d58fa1f48e6be06
metkaron <metkarongm@gmail.com>
2026-08-22 15:34:12 +0500

    Update website

--- script.js
+++ script.js
@@ -54,27 +54,24 @@ const albums = {
         category: "VIDEO",
         folder: "assets/video/",
         type: "video",
-        files: [
-            "video-01.mp4",
-            "video-02.mp4",
-            "video-03.mp4",
-            "video-04.mp4",
-            "video-05.mp4",
-            "video-06.mp4",
-            "video-07.mp4",
-            "video-08.mp4",
-            "video-09.mp4",
-            "video-10.mp4",
-            "video-11.mp4",
-            "video-12.mp4",
-            "video-13.mp4",
-            "video-14.mp4",
-            "video-15.mp4",
-            "video-16.mp4",
-            "video-17.mp4",
-            "video-18.mp4",
-            "video-19.mp4"
-        ]
+       files: [
+    "video-01.mp4",
+    "video-02.mp4",
+    "video-03.mp4",
+    "video-04.mp4",
+    "video-05.mp4",
+    "video-06.mp4",
+    "video-07.mp4",
+    "video-08.mp4",
+    "video-09.mp4",
+    "video-10.mp4",
+    "video-11.mp4",
+    "video-12.mp4",
+    "video-13.mp4",
+    "video-14.mp4",
+    "video-15.mp4",
+    "video-16.mp4"
+]
     }
 };
 
